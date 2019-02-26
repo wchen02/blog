a6e708093db5ff896e5e14a6b926ee7fef48ece4
Wensheng Chen <wensheng.chen@nytweb.com>
2019-02-26 03:52:20 -0500

    Added bootstrap for vue

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
+import BootstrapVue from 'bootstrap-vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
